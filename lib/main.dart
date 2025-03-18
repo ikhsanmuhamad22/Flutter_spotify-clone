e116d31ad3f754191bb2f5fefa2b9d8d007a8bd7
ikhsan <ikhsanmuhamad3885@gmail.com>
2025-03-18 20:13:04 +0700

    add: firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:spotify_clone/core/configs/theme/app_theme.dart';
+import 'package:spotify_clone/firebase_options.dart';
 import 'package:spotify_clone/presentation/bloc/theme_cubit.dart';
 import 'package:spotify_clone/presentation/pages/splash.dart';
 
@@ -15,6 +17,7 @@ Future<void> main() async {
             ? HydratedStorageDirectory.web
             : HydratedStorageDirectory((await getTemporaryDirectory()).path),
   );
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(MyApp());
 }
 
